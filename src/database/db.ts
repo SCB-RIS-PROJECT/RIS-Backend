ba9c1f5bbefa5142c947f2fd4f206568e259cbb1
haykal-fe-verd <haykal@verd.net.id>
2025-11-19 16:18:34 +0700

    feat: add pino logger

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import env from "@/config/env";
+import { loggerPino } from "@/config/log";
 
 const pool = new Pool({
     connectionString: env.DATABASE_URL,
@@ -8,6 +9,12 @@ const pool = new Pool({
 
 const db = drizzle({
     client: pool,
+    logger: {
+        logQuery(query, params) {
+            const logger = loggerPino;
+            logger.info({ query, params }, "Executing query");
+        },
+    },
 });
 
 export default db;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import { Hono } from "hono";
+import env from "@/config/env";
+import { PinoLogger } from "@/config/log";
 
 const app = new Hono();
 
+app.use(PinoLogger());
+
 app.get("/", (c) => {
-	return c.text("Hello Hono!");
+    return c.text("Hello Hono!");
 });
 
-export default app;
+export default {
+    port: env.PORT,
+    fetch: app.fetch,
+};
